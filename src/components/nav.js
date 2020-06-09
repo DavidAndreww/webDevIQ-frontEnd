5d8d816bc4ee1134084eab540a6770b06e939d2b
John Obertubbesing <oberthewhat@gmail.com>
2020-06-09 15:48:14 -0500

    trying to add some menu features

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,17 +6,28 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
-		flexGrow: 1,
-		backgroundColor: '#293241',
-		height: 35,
-		color: "#e0fbfc",
-		fontFamily: "'Inconsolata', monospace",
+    flexGrow: 1,
+    backgroundColor: '#293241',
+    height: 35,
+    color: "#e0fbfc",
+    fontFamily: "'Inconsolata', monospace",
   },
   menuButton: {
     marginRight: theme.spacing(2),
+    color: '#ee6c4d'
   },
   title: {
     flexGrow: 1,
@@ -24,19 +35,64 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function ButtonAppBar() {
+  const [state, setState] = React.useState({
+    right: false,
+  });
   const classes = useStyles();
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )
 
   return (
     <div>
       <AppBar position="static">
         <Toolbar className={classes.root}>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
             Web Dev Interview Questions
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button id='loginButton'>Login</Button>
+          <React.Fragment>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
+            <MenuIcon />
+          </IconButton>
+          <Drawer onClose={toggleDrawer(false)}>
+            {list}
+          </Drawer>
+          </React.Fragment>
         </Toolbar>
       </AppBar>
     </div>
