73a7e3aa05aa73909c72458aa05cb7ad35040189
John Obertubbesing <oberthewhat@gmail.com>
2020-06-09 21:43:42 -0500

    working on landing page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Nav from './components/nav'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './router'
+import Landing from './components/landing'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Nav/>   
-      <Router/>   
+        <Nav />
+        <Landing />
+        {/* <Router /> */}
       </BrowserRouter>
 
     </div>
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,11 +1,85 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grow from '@material-ui/core/Grow';
+import { makeStyles } from '@material-ui/core/styles';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
 
 
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		height: 180,
+		width: '80%'
+	},
+	container: {
+		display: 'flex',
+		flexDirection: 'column'
+	},
+	paper: {
+		margin: theme.spacing(1),
+		height: '120px',
+		width: '100%'
+	},
+	svg: {
+		width: '100%',
+		height: 100,
+	},
+}));
 
 const Landing = () => {
+	const classes = useStyles();
+	const [checked, setChecked] = React.useState(false);
 
-return (
-<div>Landing</div>
-)
+	const handleChange = () => {
+		setChecked((prev) => !prev);
+	};
+
+	return (
+		<div className='landingRoot'>
+			<div id='welcomeText'>
+				<h2>Welcome to (name).</h2>
+				<h4>(mission statement)Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4>
+			</div>
+
+			<div className={classes.root}>
+				<div className='buttonDiv'>
+				<FormControlLabel
+					control={<Switch checked={checked} onChange={handleChange} />}
+					label="Show more information"
+				/>
+				</div>
+				<div className={classes.container}>
+					<Grow in={checked}>
+						<Paper elevation={4} className={classes.paper}>
+							Some really convincing information
+						</Paper>
+					</Grow>
+					{/* Conditionally applies the timeout prop to change the entry speed. */}
+					<Grow
+						in={checked}
+						style={{ transformOrigin: '0 0 0' }}
+						{...(checked ? { timeout: 1000 } : {})}
+					>
+						<Paper elevation={4} className={classes.paper}>
+							some stuff, also convincing, maybe with a photo of a yaht
+						</Paper>
+					</Grow>
+					<Grow
+						in={checked}
+						style={{ transformOrigin: '0 0 0' }}
+						{...(checked ? { timeout: 2000 } : {})}
+					>
+						<Paper elevation={4} className={classes.paper}>
+							some stuff, mildly convincing, lets be honest, we are getting desperate by now.
+						</Paper>
+					</Grow>
+				</div>
+			</div>
+		</div>
+	)
 
 }
 
