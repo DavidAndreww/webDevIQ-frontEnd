d6d73a68452126946ce20dc4ab7744b8746f2f9c
John Obertubbesing <oberthewhat@gmail.com>
2020-06-12 22:35:17 -0500

    updated login button

--- src/components/nav.js
+++ src/components/nav.js
@@ -81,7 +81,7 @@ export default function ButtonAppBar() {
           <ListItemIcon>
             <GoGraph className='menuIcon' />
           </ListItemIcon>
-          <ListItemText primary="Scorecard" />
+          <ListItemText primary="Dashboard" />
         </ListItem>
       </List>
       <Divider />
