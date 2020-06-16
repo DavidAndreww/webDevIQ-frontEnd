1949f7891bafafafd1b760fe20dc463939f78793
John Obertubbesing <oberthewhat@gmail.com>
2020-06-15 22:59:55 -0500

    working on game

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,12 +1,115 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { QuizData } from './quizDataEx'
+import Button from '@material-ui/core/Button';
 
-const Quiz = () => {
 
-	return (
-		<div id='quizRoot'>
-				QUIZ PAGE BETCHEZZZZ!!!
-		</div>
-	)
+class Quiz extends Component {
+
+	state = {
+		userAnswer: null,
+		currentQuestion: 0,
+		options: [],
+		quizEnd: false,
+		score: 0,
+		disabled: true,
+	}
+
+	loadQuiz = () => {
+		const { currentQuestion } = this.state;
+		this.setState(() => {
+			return {
+				questions: QuizData[currentQuestion].question,
+				options: QuizData[currentQuestion].options,
+				answers: QuizData[currentQuestion].answer
+
+			}
+		}
+		)
+	}
+	componentDidMount() {
+		this.loadQuiz();
+	}
+
+	nextQuestionHandler = () => {
+		const{userAnswer, answer, score} = this.state
+		this.setState({
+			currentQuestion: this.state.currentQuestion + 1
+		})
+		if(userAnswer === answer) {
+			this.setState({
+				score: score +1
+			})
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		const { currentQuestion } = this.state
+		if (this.state.currentQuestion !== prevState.currentQuestion) {
+			this.setState(() => {
+				return {
+					disabled: true,
+					questions: QuizData[currentQuestion].question,
+					options: QuizData[currentQuestion].options,
+					answers: QuizData[currentQuestion].answer
+				}
+			})
+		}
+	}
+	checkAnswer = answer => {
+		this.setState({
+			userAnswer: answer,
+			disabled: false
+		})
+	}
+
+	finishHandler = () => {
+		if (this.state.currentQuestion === QuizData.length - 1) {
+			this.setState({
+				quizEnd: true
+			})
+		}
+	}
+
+	render() {
+		const { questions, options, currentQuestion, userAnswer, quizEnd } = this.state
+
+		if (quizEnd) {
+			return (
+				<div>
+					<h2>Gave Finished final sore is {this.state.score}</h2>
+					
+				</div>
+			)
+		}
+		return (
+			<div id='quizRoot'>
+				{questions}
+				<p>{`Question ${currentQuestion + 1} out of ${QuizData.length}`}</p>
+				{options.map(option => (
+					<Button
+						variant="contained"
+						key={option.id}
+						id={userAnswer === option ? 'selected' : null}
+						onClick={() => this.checkAnswer(option)}>
+						{option}
+					</Button>
+				))}
+				{currentQuestion < QuizData.length - 1 &&
+					<Button
+						disabled={this.state.disabled}
+						variant="contained"
+						color='primary'
+						onClick={this.nextQuestionHandler}>Next</Button>
+				}
+				{currentQuestion === QuizData.length - 1 &&
+					<Button
+						onClick={this.finishHandler}
+						variant="contained"
+						color='primary'>Finish</Button>
+				}
+			</div>
+		)
+	}
 }
 
 export default Quiz
