60f61a096b2a739a11e4fe620762b86423a36a96
John Obertubbesing <oberthewhat@gmail.com>
2020-06-16 20:24:11 -0500

    made example questions

--- src/components/quizDataEx.js
+++ src/components/quizDataEx.js
@@ -1,21 +1,21 @@
 export const QuizData = [
 	{
 		id: 0,
-		question: 'what is an iphone?',
-		options: ['surfboard', 'smartphone', 'penis', 'truck'],
-		answer: 'smartphone'
+		question: 'what is the result of:  5 + 5 + "7" ?',
+		options: ['17', '10', '557', '107'],
+		answer: '107'
 	},
 	{
 		id: 1,
-		question: 'how many rights make a left?',
-		options: ['1', '2', 'penis', '3'],
-		answer: '3'
+		question: 'Is JavaScript a case-sensitive language?',
+		options: ['Yes', 'No', 'Depends'],
+		answer: 'Yes'
 	},
 	{
 		id: 3,
-		question: 'how do you play a tuba?',
-		options: ['mouth', 'hair', 'penis', 'truck'],
-		answer: 'mouth'
+		question: 'what is the difference between: "===" , "==" , and "=" ?',
+		options: [ '"===" compares values and returns true if equal. "=" compares both value AND data type. "==" is used to assign a value to an object','"==" compares values and returns true if equal. "===" compares both value AND data type. "=" is used to assign a value to an object', '"=" compares values and returns true if equal. "==" compares both value AND data type. "===" is used to assign a value to an object'],
+		answer: '"==" compares values and returns true if equal. "===" compares both value AND data type. "=" is used to assign a value to an object'
 	},
 	{
 		id: 4,
