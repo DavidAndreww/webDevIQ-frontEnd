3afd5f3f5ac6b65057e3333ec776de4ce418d37d
John Obertubbesing <oberthewhat@gmail.com>
2020-06-18 22:56:42 -0500

    no need for use styles on quiz, yet

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -2,18 +2,9 @@ import React, { Component } from 'react'
 import { QuizData } from './quizDataEx'
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom'
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-	link: {
-		textDecoration: 'none'
-	}
-});
 
 
 class Quiz extends Component {
-
-
 
 	state = {
 		userAnswer: null,
