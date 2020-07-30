6e63efdb0a56accb7d5174332f9568493a73f1cd
unknown <d_bille@yahoo.com>
2020-07-30 17:17:52 -0500

    added email/password keys to user Object

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,6 +1,8 @@
 const state = {
     userObject: {
         id: 1,
+        email: 'toneZone@aol.com',
+        password: 'toneCalzone123',
         username: 'Test User #1',
         questionsAttempted: 224,
         correctAnswers: 200
